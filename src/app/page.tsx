4ca89dc8521e0e7d71a3e654dffd797a11bdf773
Aditya-PS-05 <adityapratapsjnhh7654@gmail.com>
2025-06-30 12:54:57 +0530

    feat: add ai-agents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,31 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Toaster } from "sonner";
+import { Input } from "@/components/ui/input";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { toast } from "sonner";
 
 const Page = () =>{
+
+  const [value, setValue] = useState<string>("");
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess: () => {
+      toast.success("Background job started");
+    }
+  }))
   
   return (
-    <div>
-      <Button></Button>
-      Test
+    <div className="p-4 max-w-7xl mx-auto">
+        <Input value={value} onChange={(e) => setValue(e.target.value)} />
+        <Button
+          disabled={invoke.isPending}
+          onClick={() => invoke.mutate({value: value})}
+        >
+          Invoke
+          </Button>
     </div>
   );
 }
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,23 @@
 import { inngest } from "./client";
+import { anthropic, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+
+    // Create a new agent with a system prompt (you can add optional tools, too)
+    const codeAgent = createAgent({
+      name: "summarizer",
+      system: "You are an expert next.js and react-native developer. You write the readable, maintainable code. You write simple as well as complex Next.js, React.js and React-Native snippets",
+      model: anthropic({ model: "claude-3-5-haiku-20241022", defaultParameters: { max_tokens: 100 } }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Summarize the following text: ${event.data.value}`,
+    );
+
+    console.log(output);
+    return {output}
   },
 );

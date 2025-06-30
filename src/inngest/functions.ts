3168bb17c041d3a2086a2b85e0a2675a470ea5e7
Aditya-PS-05 <adityapratapsjnhh7654@gmail.com>
2025-06-30 20:06:25 +0530

    feat: add ai-tools

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,7 +1,9 @@
 import { inngest } from "./client";
-import { anthropic, createAgent } from "@inngest/agent-kit";
+import { anthropic, createAgent, createNetwork, createTool } from "@inngest/agent-kit";
 import {Sandbox} from "@e2b/code-interpreter"
-import { getSandboxId } from "./utils";
+import { getSandbox, lastAssistantTextMessageContent } from "./utils";
+import { z } from "zod";
+import { PROMPT } from "@/prompt";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -10,27 +12,146 @@ export const helloWorld = inngest.createFunction(
 
     const sandboxId = await step.run("get-sandbox-id", async () => {
       const sandbox = await Sandbox.create("novacraft-nextjs-test")
-      return sandbox.sandboxId
+      return sandbox.sandboxId;
     })
 
-    // Create a new agent with a system prompt (you can add optional tools, too)
     const codeAgent = createAgent({
-      name: "summarizer",
-      system: "You are an expert next.js and react-native developer. You write the readable, maintainable code. You write simple as well as complex Next.js, React.js and React-Native snippets",
-      model: anthropic({ model: "claude-3-5-haiku-20241022", defaultParameters: { max_tokens: 100 } }),
+      name: "codeAgent",
+      description: "You are an expert next.js developer",
+      system: PROMPT,
+      model: anthropic({ model: "claude-3-5-sonnet-latest", defaultParameters: { max_tokens: 3000, temperature: 0.1 } }),
+      tools: [
+        createTool({
+          name: "terminal",
+          description: "Use the terminal to run the commands",
+          parameters: z.object({
+            command: z.string(),
+          }),
+          handler: async ({command}, {step}) => {
+            return await step?.run("terminal", async () => {
+              const buffers = {stdout: "", stderr: ""};
+
+              try {
+                const sandbox = await getSandbox(sandboxId);
+                const result = await sandbox.commands.run(command, {
+                  onStdout: (data: string) => {
+                    buffers.stdout += data;
+                  },
+                  onStderr: (data: string) => {
+                    buffers.stderr+= data;
+                  },
+                })
+                return result.stdout;
+              } catch (error) {
+                console.error(
+                  `Command failed: ${error} \nstdout: ${buffers.stdout} \nstderr: ${buffers.stderr}`
+                );
+                return `Command failed: ${error} \nstdout: ${buffers.stdout} \nstderr: ${buffers.stderr}`;
+              }
+            })
+          }
+        }),
+        createTool({
+          name: "createOrUpdateFiles",
+          description: "Create or update files in the sandbox",
+          parameters: z.object({
+            files: z.array(
+              z.object({
+                path: z.string(),
+                content: z.string(),
+              })
+            )
+          }),
+          handler: async (
+            {files},
+            {step, network}
+          )  => {
+            const newFiles = await step?.run("createOrUpdateFiles", async () => {
+              try {
+                const updatedFiles = network.state.data.files || {};
+                const sandbox = await getSandbox(sandboxId);
+
+                for(const file of files) {
+                  await sandbox.files.write(file.path, file.content);
+                  updatedFiles[file.path] = file.content;
+                }
+
+                return updatedFiles;
+              } catch (error) {
+                return `Error: ${error}`
+              }
+            })
+
+            if (typeof(newFiles) === "object") {
+              network.state.data.files = newFiles;
+            }
+          }
+        }),
+        createTool({
+          name: "readFiles",
+          description: "Read files frpm the sandbox",
+          parameters: z.object({
+            files: z.array(z.string()),
+          }),
+          handler: async ({files}, {step}) => {
+            return await step?.run("readFiles", async () => {
+              try {
+                const sandbox = await getSandbox(sandboxId);
+                const contents = [];
+
+                for(const file of files) {
+                  const content = await sandbox.files.read(file);
+                  contents.push({path: file, content});
+                }
+
+                return JSON.stringify(contents);
+              } catch (error) {
+                return "Error: " + error;
+              }
+            })
+          }
+        })
+      ],
+      lifecycle: {
+        onResponse: async ({result, network}) => {
+          const lastAssistantTextMessageText = lastAssistantTextMessageContent(result);
+
+          if(lastAssistantTextMessageText && network) {
+            if(lastAssistantTextMessageText.includes("<task_summary>")) {
+              network.state.data.summary = lastAssistantTextMessageText;
+            }
+          }
+          return result;
+      },
+    },
     });
 
-    const { output } = await codeAgent.run(
-      `Summarize the following text: ${event.data.value}`,
-    );
+    const network = createNetwork({
+      name: "coding-agent-network",
+      agents: [codeAgent],
+      maxIter: 15,
+      router: async ({network}) => {
+        const summary = network.state.data.summary;
+        if(summary) return;
+
+        return codeAgent;
+      }
+    })
+
+    const result = await network.run(event.data.value);
 
     const sandboxUrl = await step.run("get-sandbox-url", async () => {
-      const sandbox = await getSandboxId(sandboxId);
+      const sandbox = await getSandbox(sandboxId);
       const host= sandbox.getHost(3000);
 
       return `https://${host}`;
     })
 
-    return {output, sandboxUrl}
+    return {
+      url:sandboxUrl,
+      title: "Fragment",
+      files: result.state.data.files,
+      summary: result.state.data.summary
+    }
   },
 );
